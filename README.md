8c09af746abe02ac28255df0e30bc17bcc4376ec
Uriel Campos <urieldecampos@gmail.com>
2024-06-08 20:14:41 -0300

    Added plotly bar charts for "Turno" and "Lucro"

---
+++ README.md
@@ -0,0 +1,16 @@
+# Comodoro Analysis
+
+This project was created for Pomodoro Company to perform a detailed data analysis on business finances.
+
+
+## Install dependencies
+
+```bash
+  pip install streamlit plotly pandas
+```
+    
+## Run streamlit
+
+```bash
+  stramlit run comodoro.py
+```
--- comodoro.py
+++ comodoro.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 if 'data' not in st.session_state:
-    df = pd.read_excel(r'C:\Users\user\Downloads\Data science\Python\Projetos do Pirão\projeto_comodoro\Pasta1.xlsx')
+    df = pd.read_excel(r'Pasta1.xlsx')
     df['Status'] = df['Status'].fillna('Não')
     df['Turno'] = df['Turno'].fillna('Não')
     st.session_state['data'] = df
@@ -16,7 +17,27 @@ df = st.session_state['data']
 df_entregas =  df[df['Produto'] == 'entregas']
 st.dataframe(df_entregas)
 
+df_filtrado = df[['Turno', 'Lucro']]
+df_filtrado = df_filtrado[df_filtrado['Turno'].str.strip() != 'Não']
+df_filtrado['Lucro'] = df_filtrado['Lucro'].astype(str).replace('[^\d,.-]', '', regex=True).str.replace(',', '.').astype(float)
+df_filtrado = df_filtrado.sort_values(by='Turno')
+fig_df_filtrado = px.bar(df_filtrado, x='Turno', y='Lucro', title='Lucro por Turno Geral')
+st.plotly_chart(fig_df_filtrado)
+
 restaurante = df_entregas['Restaurante'].value_counts().index
 restaurantes = st.selectbox('Restaurante', restaurante)
 df_rest = df[df['Restaurante'] == restaurantes]
+
 df_rest
+
+df_rest_filtrado = df_rest[['Turno', 'Lucro']]
+
+df_rest_filtrado = df_rest_filtrado[df_rest_filtrado['Turno'].str.strip() != 'Não']
+
+df_rest_filtrado['Lucro'] = df_rest_filtrado['Lucro'].astype(str).replace('[^\d,.-]', '', regex=True).str.replace(',', '.').astype(float)
+
+df_filtrado = df_rest_filtrado.sort_values(by='Turno')
+
+fig_df_rest_filtrado = px.bar(df_filtrado, x='Turno', y='Lucro', title='Lucro por Turno')
+
+st.plotly_chart(fig_df_rest_filtrado)
